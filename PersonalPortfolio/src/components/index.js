a57b97a7ecffc656e97d8170ea4634980ee1b879
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-06-02 23:29:28 +0500

    First PortFolio Deleted

--- PersonalPortfolio/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar/Navbar"
-import Footer from "./Footer/Footer"
-import Home from "./Homes/Home"
-import Projects from "./Projects/Projects"
-import AboutUs from "./AboutUs/AboutUs"
-import Applications from "./Applications/Applications"
-
-
-export {Navbar, Footer, Home, Applications, AboutUs, Projects}
