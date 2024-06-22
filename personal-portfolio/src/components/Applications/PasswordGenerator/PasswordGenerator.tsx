2a88244f421b6795c086a900d2654b63c1a1949c
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-06-22 20:26:24 +0500

    Update PasswordGenerator.tsx

--- personal-portfolio/src/components/Applications/PasswordGenerator/PasswordGenerator.tsx
+++ personal-portfolio/src/components/Applications/PasswordGenerator/PasswordGenerator.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import {Slider, Button, Checkbox} from "@nextui-org/react";
+'use client'
 
+import React, {useState, useEffect} from 'react'
+import {Slider, Button, Checkbox} from "@nextui-org/react";
+ 
 function PasswordGenerator() {
+  const [password, setPassword] = useState<string>("")
+  const [Length, setLength] = useState<number | any>(8)
+  const [isnumber, setIsNumber] = useState<boolean>(false)
+  const [isUppercase, setIsUppercase] = useState<boolean>(false)
+  const [isSpecialChar, setIsSpecialChar] = useState<boolean>(false)
+
+
+  ;(()=>{
+    let passwordStr = "abcdefghijklmnopqrstwxyz"
+    const pass_Uppercase = "ABCDEFGHIJKLMNOPQRSTWXYZ"
+    const pass_number = "0123456789"
+    const pass_specialChar = "!@#$%^&*():'}{[]?.,"
+    
+
+    useEffect(()=>{
+      if (isnumber){
+        passwordStr += pass_number
+      }
+      if (isUppercase){
+        passwordStr += pass_Uppercase
+      }
+      if (isSpecialChar){
+        passwordStr += pass_specialChar
+      }
+      
+
+    }, [isnumber, isUppercase, isSpecialChar, Length, password])
+
+
+  })();
+  // useEffect(()=>{
+  //   const passwordStr = "abcdefghijklmnopqrstwxyz"
+
+  //   console.log(Length)
+  // }, [isnumber, isUppercase, isSpecialChar, Length, password])
+  
+
   return (
     <div className='w-full flex justify-center items-center mt-12'>
-        <div className='h-auto w-[580px] bg-gray-900 rounded-xl p-5 box-border space-y-6 shadow-sm shadow-gray-950'>
+        <div className='h-auto w-[580px] bg-gray-900 rounded-xl p-5 box-border space-y-6 shadow-s=>m shadow-gray-950'>
 
           {/* ===== Heading ==== */}
           <h1 className='text-center text-2xl font-bold mb-9'> 
@@ -29,7 +68,8 @@ function PasswordGenerator() {
               step={1} 
               maxValue={16} 
               minValue={8} 
-              defaultValue={8}
+              value={Length}
+              onChange={(value)=>setLength(value)}
               className="max-w-md"
             />
             </div>
@@ -39,9 +79,20 @@ function PasswordGenerator() {
           {/* ===== CheckBox Options ===== */}
           <div className='w-full flex justify-center'>
             <div className='w-[430px]'>
-              <Checkbox defaultSelected color="secondary" size='sm'>Numbers</Checkbox> <br />
-              <Checkbox defaultSelected color="primary" size='sm'>UpperCase</Checkbox> <br />
-              <Checkbox defaultSelected color="success" size='sm'>Special Chars</Checkbox>
+              <Checkbox  color="secondary" size='sm'
+              isSelected = {isnumber}
+              onChange={()=>setIsNumber((prev)=>!prev)}
+              >Numbers</Checkbox> <br />
+
+              <Checkbox defaultSelected color="primary" size='sm'
+              isSelected={isUppercase}
+              onChange={()=>setIsUppercase((prev)=>!prev)}
+              >UpperCase</Checkbox> <br />
+
+              <Checkbox defaultSelected color="success" size='sm'
+              isSelected={isSpecialChar}
+              onChange={()=>setIsSpecialChar((prev)=>!prev)}
+              >Special Chars</Checkbox>
             </div>
 
           </div>
